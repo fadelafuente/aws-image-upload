18db64e8c263c035e3bddb655aac6e13fdce6d38
fadelafuente <f.delafuente@outlook.com>
2023-07-28 14:55:12 -0500

    add upload image API

--- src/main/java/com/fadelafuente/awsimageupload/profile/UserProfileService.java
+++ src/main/java/com/fadelafuente/awsimageupload/profile/UserProfileService.java
@@ -2,8 +2,10 @@ package com.fadelafuente.awsimageupload.profile;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserProfileService {
@@ -16,5 +18,14 @@ public class UserProfileService {
 
     List<UserProfile> getUserProfiles() {
         return userProfileDataAccessService.getUserProfiles();
+    }
+
+    public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+        // 1. Check if image is not empty
+        // 2. Check if image file is an image
+        // 3. Check whether the user exists in database
+        // 4. Grab some metadata from file if any
+        // 5. Store the image in s3 and update database
+        //    userProfileImageLink with s3 image link
     }
 }
