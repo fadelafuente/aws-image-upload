aba0d23b7db6217164f50579b6757cab25e3a021
fadelafuente <f.delafuente@outlook.com>
2023-07-31 11:33:39 -0500

    add initial react app with access to backend

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from "axios";
+import React, {useState, useEffect} from 'react';
+
+const UserProfiles = () => {
+  const fetchUserProfiles = () => {
+    axios.get("http://localhost:8080/api/v1/user-profile").then(res => {
+      console.log(res);
+    });
+  };
+
+  useEffect(() => {
+    fetchUserProfiles();
+  }, []);
+
+  return <h1>hello</h1>;
+};
+
+
+function App() {
+  return (
+    <div className="App">
+      <UserProfiles />
+    </div>
+  );
+}
+
+export default App;
